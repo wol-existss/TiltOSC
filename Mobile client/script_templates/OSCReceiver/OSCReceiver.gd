54a8d8295f86e216b5c8f4781562253f81be8187
wol-existss <wol.chan@icloud.com>
2026-08-01 17:47:22 -0400

    Added GodOSC plugin

---
+++ Mobile client/script_templates/OSCReceiver/OSCReceiver.gd
@@ -0,0 +1,14 @@
+#meta-name: OSCReceiver Default
+# meta-description: Base template for an OSCReceiver script
+# meta-default: true
+
+extends _BASE_
+
+
+## Code to be ran when Parent Control is set to custom.
+func _custom_control(address : String, vals : Array, time):
+	
+	if vals != []:
+		#put your code here. This if statement prevents your code from being ran if you receive an empty message
+		pass
+	pass
