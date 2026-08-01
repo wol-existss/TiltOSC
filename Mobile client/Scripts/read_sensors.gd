be7bb5185f95acef4927e56815c173fe714437ee
wol-existss <wol.chan@icloud.com>
2026-08-01 00:12:50 -0400

    Added debug mode; Wrapped values. Needs work due to persistent cross-talk issues.

--- Mobile client/Scripts/read_sensors.gd
+++ Mobile client/Scripts/read_sensors.gd
@@ -1,5 +1,9 @@
 extends Node
+@export var debug_output = true
 var frames = 0
+var wheel_angle = 0
+var pitch_angle = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -9,6 +13,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var g = Input.get_gravity()
 	frames = frames + 1
-	if frames >= 60:
-		print(Input.get_accelerometer())
+	wheel_angle = wrapf((atan2(g.y, g.x) / PI) + 0.5, -1.0, 1.0)
+	pitch_angle = wrapf((atan2(g.y, g.z) / PI) + 0.5, -1.0, 1.0)
+
+	if frames >= 10 and debug_output:
+		print(g)
+		print(wheel_angle)
+		print(pitch_angle)
 		frames = 0
