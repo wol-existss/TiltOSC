142533a9afd92b223c9423dd04ef1ca2e55f2e01
wol-existss <wol.chan@icloud.com>
2026-08-09 14:40:45 -0400

    Update read_sensors.gd

--- Mobile client/Scripts/read_sensors.gd
+++ Mobile client/Scripts/read_sensors.gd
@@ -8,9 +8,9 @@ var pitch_angle = 0
 @onready var wheel_message = $"../WheelMessage"
 @onready var pitch_message = $"../PitchMessage"
 # Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	pass
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
