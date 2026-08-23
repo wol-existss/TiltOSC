7b3c1e37883b5d67290b04ba08b4deece4fe4661
wol-existss <wol.chan@icloud.com>
2026-08-23 02:50:11 -0400

    Update main scene directory

--- Mobile client/Scripts/preferences.gd
+++ Mobile client/Scripts/preferences.gd
@@ -131,4 +131,4 @@ func _on_kill() -> void:
 	get_tree().quit() 
 
 func _on_main_menu_pressed() -> void:
-	get_tree().change_scene_to_file("res://Main.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Main.tscn")
