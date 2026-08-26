3db847a655ef5b9ada2fa3ae83d991ca9442d9d5
wol-existss <wol.chan@icloud.com>
2026-08-25 22:39:12 -0400

    Added widget save/load functions

--- Mobile client/Scripts/widget_manager.gd
+++ Mobile client/Scripts/widget_manager.gd
@@ -5,13 +5,20 @@ extends Node
 @export var button_scene: PackedScene
 
 # Debug
-@export var master_debug = false
+@export var master_debug = false # Enables all debug flags if enabled
 @export var widget_debug = true
+@export var save_load_debug = true
 
 var widgets = [] # Widget array
 
 func _ready() -> void:
 	var new_id = add_widget("button")
+	
+	# Debug
+	if master_debug:
+		widget_debug = true
+		save_load_debug = true
+	
 	if widget_debug:
 		print("Addded widget with ID:", new_id)
 		print("Current widgets: ", widgets)
@@ -26,13 +33,11 @@ func add_widget(type: String) -> int:
 	var widget = {"id": new_id, "type": type, "value": 0.0} ## Forms dictionary
 	widgets.append(widget) ## Adds entry to end of array
 	
-	
-	
 	# Add button
 	if type == "button":
 		var new_button = button_scene.instantiate()
 		widget_canvas.add_child(new_button)
-		new_button.position = Vector2(50, 50)
+		new_button.position = Vector2(widget["x"], widget["y"])
 	
 	return new_id
 
@@ -42,3 +47,19 @@ func remove_widget(id: int) -> void:
 		if widgets[i]["id"] == id:
 			widgets.remove_at(i)
 			return
+
+# Save widgets
+func save_widgets() -> void:
+	var json_string = JSON.stringify(widgets) # Converts array into JSON
+	var file = FileAccess.open("user://widgets.json", FileAccess.WRITE) # Create or open the existing JSON file for writing.
+	file.store_string(json_string) # Push modifications
+	if save_load_debug:
+		print("Widgets saved")
+
+# Load widgets
+func load_widgets() -> void:
+	var file = FileAccess.open("user://widgets.json", FileAccess.READ) # Open saved file for reading
+	var json_string = file.get_as_text() # Extract the raw text
+	widgets = JSON.parse_string(json_string) # Converts the raw text back into array of dictionaries
+	if save_load_debug:
+		print("Widgets loaded")
