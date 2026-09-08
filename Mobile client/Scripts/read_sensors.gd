900d4a81ad6dc54b66b1778e4b91855ecaaf7860
wol-existss <wol.chan@icloud.com>
2026-09-08 14:51:38 -0400

    Remove the no_sensor_send value to avoid release issues

--- Mobile client/Scripts/read_sensors.gd
+++ Mobile client/Scripts/read_sensors.gd
@@ -1,16 +1,22 @@
 extends Node
+
 # Debug
 @export var debug_output = false
 @export var debug_output_buffer = 100
-@export var no_sensor_send = false
 
 # OSC sender nodes
 @export var gravity: Node
 @export var gyro: Node
+@export var init: Node
 var frames = 0
 
+var ipv4 = IP.get_local_addresses()
+
 func _ready() -> void:
-	LoadNetworkConfig.load_network_conf($"../OSC/OSCClient") 
+	LoadNetworkConfig.load_network_conf($"../OSC/OSCClient")
+	 
+	init.send_message(["connected"])
+	init.send_message(ipv4)
 
 func _process(delta: float) -> void:
 	var g = Input.get_gravity()
