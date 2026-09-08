0e0294ae3abba51756069a4d949bfd8e476ebaec
wol-existss <wol.chan@icloud.com>
2026-09-08 16:22:56 -0400

    Update environment.h

--- Launcher/App/autogen/environment.h
+++ Launcher/App/autogen/environment.h
@@ -8,6 +8,7 @@
 #include <QGuiApplication>
 #include "qqmlextensionplugin.h"
 
+Q_IMPORT_QML_PLUGIN(LauncherContentPlugin)
 Q_IMPORT_QML_PLUGIN(LauncherPlugin)
 Q_IMPORT_QML_PLUGIN(LauncherContentPlugin)
 
